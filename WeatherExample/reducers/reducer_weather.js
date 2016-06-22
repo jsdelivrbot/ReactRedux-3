8590593fde89cc60b0beea45a73ecc0613361be6
maninder123 <manindersingh221@gmail.com>
2016-06-22 21:21:51 +0530

    change in request sending in action

--- WeatherExample/reducers/reducer_weather.js
+++ WeatherExample/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions/index'
 export default function(state = null ,action){
            switch(action.type){
           case FETCH_WEATHER:
-              return [action.payload]
+              return [action.payload.data]
       }
       return state;
 }
